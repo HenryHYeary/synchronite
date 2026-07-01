a733d0150bba46747bb98796af557b2f4b419c7e
HenryHYeary <HenryHYeary@github.com>
2026-06-30 21:57:38 -0500

    Worked more on watcher change event listener, need to work on sync engine next.

--- src/sync/watcher.ts
+++ src/sync/watcher.ts
@@ -1,7 +1,7 @@
 import chokidar from "chokidar";
 import { Config } from "../config";
-import { loadIndex } from "../state";
-import fs from "fs";
+import { loadIndex, updateIndex } from "../state";
+import path from "path";
 
 const WATCH_PATTERNS = [
     "**/*.srm",
@@ -26,7 +26,16 @@ export async function runWatcher(config: Config) {
     },
   );
 
-  watcher.on("add", (path) => {
+  watcher.on("change", async (filePath) => {
     const index = loadIndex();
+    const updated = await updateIndex(index, filePath);
+
+    if (!updated) {
+      throw new Error("Failed to update index.");
+    }
+
+    // TODO: implement syncFile in sync engine module
+    // Messy code below, need to find a better way to do this
+    // await syncFile(filePath, path.relative(path.normalize(`${config.retroarchSaveDir}/..`), filePath));
   });
 }
